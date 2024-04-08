dac53ec2449032efa4f010d04a0c03dcd33f1ab3
Realwale <olawaleagboola41@gmail.com>
2024-04-08 11:11:04 +0100

    initialize project

---
+++ src/main/java/com/backend/wavault/WavaultApplication.java
@@ -0,0 +1,13 @@
+package com.backend.wavault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WavaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WavaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/wavault/WavaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.wavault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WavaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
